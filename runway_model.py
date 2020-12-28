2e61f3c4e032567e4a20b2722ec087aaaa0f68ec
ak9250 <ahsen.khaliq@gmail.com>
2020-12-28 14:07:06 -0500

    changes for densepose

--- runway_model.py
+++ runway_model.py
@@ -22,16 +22,17 @@ def visualize(model, inputs):
   im = np.array(inputs['input'])
   cfg = get_cfg()
   # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-  cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+  cfg.merge_from_file(model_zoo.get_config_file("densepose_rcnn_R_50_FPN_s1x.yaml"))
   cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
   # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-  cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+  cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("densepose_rcnn_R_50_FPN_s1x.yaml")
   predictor = DefaultPredictor(cfg)
   outputs = predictor(im)
   # We can use `Visualizer` to draw the predictions on the image.
   v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
   out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
   return out.get_image()[:, :, ::-1]
+
 
 if __name__ == '__main__':
   runway.run()
