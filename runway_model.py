2809e29c08c333b7e3a53c34bddc4c5892c4b8ac
ak9250 <ahsen.khaliq@gmail.com>
2020-12-29 23:09:28 -0500

    add model

--- runway_model.py
+++ runway_model.py
@@ -7,7 +7,7 @@ import numpy as np
 import os, json, cv2, random
 
   
-@runway.command('visualize', inputs={'input': runway.image}, outputs={'output': runway.image})
+@runway.command('visualize', inputs={'input': runway.image(description='input image')}, outputs={'output': runway.image(description='output image')})
 def visualize(model, inputs):
   im = np.array(inputs['input'])
   
@@ -16,9 +16,9 @@ def visualize(model, inputs):
   out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
   return out.get_image()[:, :, ::-1]
 
-@runway.command('visualize2', inputs2={'input2': runway.image}, outputs={'output2': runway.image})
-def visualize2(model, inputs2):
-  im = np.array(inputs2['input2'])
+@runway.command('visualize2', inputs={'input2': runway.image(description='input image')}, outputs={'output2': runway.image(description='output image')})
+def visualize2(model, inputs):
+  im = np.array(inputs['input2'])
   outputs = developset.predictorkey(im)
   v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(developset.cfgkey.DATASETS.TRAIN[0]), scale=1.2)
   out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
